83755784d29a7e870918933aedd608c60330e2d8
rogulik <malvla2008@gmail.com>
2025-03-26 11:40:56 +0300

    Login feature added.

---
+++ Application/Features/Exceptions/UsersPasswordIsInvalid.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Features.Exceptions
+{
+    public class UsersPasswordIsInvalid()
+        : Exception("Provided password is invalid.")
+    { }
+}
---
+++ Application/Features/User/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Auth;
+using Application.Features.Exceptions;
+using Application.UnitOfWork;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Features.User.Commands.LoginUser
+{
+    public class LoginUserCommandHandler(
+        IUnitOfWork unitOfWork, IPasswordHasher<Domain.User.User> passwordHasher,
+        IAuthService auth)
+        : IRequestHandler<LoginUserCommand, string>
+    {
+        public async Task<string> Handle(
+            LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await unitOfWork.UserRepository
+                .GetUserByIdAsync(request.userId);
+
+            if (user == null)
+            {
+                throw new UserNotFound(request.userId);
+            }
+
+            var verificationResult = passwordHasher.VerifyHashedPassword(
+                user, user.Password, request.password);
+
+            if (verificationResult == PasswordVerificationResult.Failed)
+            {
+                throw new UsersPasswordIsInvalid();
+            }
+
+            var token = auth.GenerateToken(user);
+
+            return token;
+        }
+    }
+}
